ae5c729df220f0409bf407c53c842e2cd626e311
DELL <DELL@DESKTOP-HIS8892>
2021-07-04 18:25:33 +0530

    CarController

---
+++ CarsDemo/src/test/java/com/car/controller/CarsDemo/CarsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.controller.CarsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
